3fdff1c76653e8c57d03eda4b0b4a0eca5a796d8
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-07-22 09:54:46 +0800

    add 2 routes (index and new)

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('node:path');
 const app = express();
+const indexRouter = require('./routes/indexRouter');
+const newMessageRouter = require('./routes/newMessageRouter');
 
 const PORT = 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/', indexRouter);
+app.use('/new', newMessageRouter);
 
 app.use((err, req, res, next) => {
   console.error(err);
